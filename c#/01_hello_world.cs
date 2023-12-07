c4cfe592ca2afd7bb9f272cc6f5d7d3127f5a554
cdeveloping <carlosdeveloper@outlook.es>
2023-12-07 22:40:57 +0100

    C#
    Comenzando lo básico de C#

---
+++ c#/01_hello_world.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+public class Class1
+{
+	public Class1()
+	{
+		//Este archivo es a modo de ejemplo como se comienza en C# y está basado en el curso gratuito de Microsoft que puedes encontrar en su web.
+
+		/*
+		 * Como en cualquier lenguaje de programación, se comineza con el hello World.
+		 */
+
+		Console.WriteLine("Hello World!!"); //Esto nos dará una salida por consola con ese texto.
+
+			//Tenemos que tener presente que C# es sensible a las mayusculas, por lo que debemos prestar atención a la hora
+
+		/*
+		 * Si escribimos console.WriteLine tendremos un error
+		 */
+
+			console.WriteLine("Esto nos va a dar error");
+	}
+}
