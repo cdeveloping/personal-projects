061aff9b32a15e1780dd496eb0257fd1c09d3335
cdeveloping <carlosdeveloper@outlook.es>
2024-02-07 21:25:55 +0100

    Iniciando Js
    Inicios Js Variables y mostrar información en página web

---
+++ js/archivo.js
@@ -0,0 +1,21 @@
+/**
+ * Author: Carlos G. Pascual
+ * Curso Js.
+ * Date: 07/02/2024
+ * 
+ */
+
+//document.write("Esto es código externo");
+
+//Variables
+/*var nombre;
+
+nombre = "Carlos";
+
+document.write(nombre);*/
+
+let _nombre = "Carlos 2";
+document.write(_nombre);
+
+
+
