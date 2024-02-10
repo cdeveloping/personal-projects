5267e9c61b366f92ef48207d4ba0d2ecc828fa4f
Carlos <carlosdeveloper@outlook.es>
2024-02-10 18:15:45 +0100

    Operadores de asignación

--- js/Curso Js/archivo.js
+++ js/Curso Js/archivo.js
@@ -14,9 +14,36 @@ nombre = "Carlos";
 
 document.write(nombre);*/
 
-
+/*
 let _nombre = "Carlos 2";
 document.write(_nombre);
+*/
+let a = 1;
+document.write("El valor de a es en este punto: " + a);
+document.write('<br>');
+document.write('Vamos a incrementar el valor de a usando ++');
+document.write('<br>');
+a++;
+document.write("El valor de a ahora es: " + a);
+document.write('<br>');
+document.write('<hr>');
 
+document.write("Vamos a aumentar a en base al valor que introduzcamos");
+document.write('<br>');
+a+=parseInt(prompt("Introduce un valor para a"));
+document.write('El valor de a ahora es: ' + a);
+document.write('<hr>');
+document.write('<br>');
+
+document.writeln('Vamos a reducir el valor de a usando --');
+document.write('<br>');
+a--;
+document.writeln('El valor de a en este punto es: ' + a);
+document.writeln('<hr>')
+
+document.write('Vamos a reducir en 1 el valor de a con -=')
+document.write('<br>');
+a-=1;
+document.write('El valor de a en este punto es: ' + a);
 
 
