64edad761f7b2688c94398656f2e83107eed339a
Carlos <carlosdeveloper@outlook.es>
2024-02-24 09:54:32 +0100

    While

---
+++ js/Curso Js/Js/while.js
@@ -0,0 +1,22 @@
+/**
+ * Name: While
+ * Author: Carlos Pascual
+ * mail: cguillermodev@gmail.com
+ * date: 24/02/2024
+ * Version: 1.0
+ * 
+ * Last update: 24/02/2024
+ * Last Version: 24/02/2024
+ */
+
+
+let f = 1;
+
+while (f <= 10) {
+    document.write('Valor actual de f: ', f);
+    document.write('<br>');
+    f++;
+}                 
+
+
+document.write('<br><hr>')
