e39505217f7795db85f175f7ea7fd766af89433a
Carlos <carlosdeveloper@outlook.es>
2024-02-27 21:49:04 +0100

    Break-continue

---
+++ js/Curso Js/Js/break-continue.js
@@ -0,0 +1,11 @@
+/**
+ * Name: Bucle Break-continue
+ * Author: Carlos Pascual
+ * mail: cguillermodev@gmail.com
+ * Date: 27/02/2024
+ * Version: 1.0ç
+ * 
+ * Last update: 27/02/2024
+ * last version: 1.0
+ */
+
