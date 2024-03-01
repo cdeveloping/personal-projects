095f26045264ce6daeca2e4c9277568c773b8823
Carlos <carlosdeveloper@outlook.es>
2024-03-01 19:05:03 +0100

    POO
    Primeros pasos en la programación orientada a objetos con Js

---
+++ js/Curso Js/Js/POO_objetos.js
@@ -0,0 +1,23 @@
+/**
+ * Objetos en Js // Programación Orientada a objetos
+ */
+
+var Auto = {
+    marca: 'Seat',
+    modelo: 'Tarraco',
+    anio: 2022,
+    color: 'Azul',
+    tipo: 'SUV'
+}
+document.write('Creamos el objeto Auto');
+document.write('<br>');
+document.write('Marca: ', Auto.marca);
+document.write('<br>')
+document.write('Modelo: ', Auto.modelo);
+document.write('<br>');
+document.write('Año: ', Auto.anio);
+document.write('<br>');
+document.write('Color: ', Auto.color);
+document.write('<br>');
+document.write('Tipo: ', Auto.tipo);
+document.write('<br>');
